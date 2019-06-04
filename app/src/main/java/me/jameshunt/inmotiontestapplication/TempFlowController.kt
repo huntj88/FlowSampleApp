ff13a2d9307c25d9b319c552216ed338c662433b
huntj88 <huntj88@gmail.com>
2019-06-03 21:28:23 -0500

    bridge between standard activities and Flow

--- app/src/main/java/me/jameshunt/inmotiontestapplication/TempFlowController.kt
+++ app/src/main/java/me/jameshunt/inmotiontestapplication/TempFlowController.kt
@@ -1,5 +1,8 @@
 package me.jameshunt.inmotiontestapplication
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.provider.MediaStore
 import com.inmotionsoftware.promisekt.Promise
 import com.inmotionsoftware.promisekt.ensure
 import com.inmotionsoftware.promisekt.thenMap
@@ -30,6 +33,32 @@ class TempFlowController : GeneratedTempController() {
     override fun onLoad(state: Load): Promise<FromLoad> {
         return Promise.value(Unit)
             .ensure(on = DispatchExecutor.background) { Thread.sleep(3000) }
-            .thenMap { state.toTemp() }
+            .thenMap { state.toActivity() }
+    }
+
+    override fun onActivity(state: Activity): Promise<FromActivity> {
+
+        val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+        getIntent.type = "image/*"
+
+        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickIntent.type = "image/*"
+
+        val chooserIntent = Intent.createChooser(getIntent, "Select Image")
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+        val bitmap = this.flow<Bitmap>(chooserIntent) { context, data ->
+            MediaStore.Images.Media.getBitmap(context.contentResolver, data!!.data)
+        }
+
+        return bitmap.forResult(
+            onComplete = {
+                println("woohoooooo bitmap: ${it.allocationByteCount}")
+                state.toTemp()
+            },
+            onBack = {
+                println("back from activity")
+                state.toTemp()
+            })
     }
 }
