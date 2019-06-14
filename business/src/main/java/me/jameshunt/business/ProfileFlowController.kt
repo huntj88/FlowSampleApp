2703b89193b2cbbb906d439d2b252ad5f863a43d
huntj88 <huntj88@gmail.com>
2019-06-13 19:15:20 -0500

    business flow generation example

--- business/src/main/java/me/jameshunt/business/ProfileFlowController.kt
+++ business/src/main/java/me/jameshunt/business/ProfileFlowController.kt
@@ -1,25 +1,31 @@
 package me.jameshunt.business
 
 import com.inmotionsoftware.promisekt.Promise
-import me.jameshunt.flowcore.BusinessFlowController
+import me.jameshunt.flow.generated.GeneratedProfileController
+import me.jameshunt.flow.generated.GeneratedProfileController.ProfileFlowState.*
 
-class ProfileBusinessFlowController : BusinessFlowController<Unit, Profile>() {
-    override fun onStart(state: InitialState<Unit>) {
+class ProfileBusinessFlowController: GeneratedProfileController() {
+    override fun onGetProfile(state: GetProfile): Promise<FromGetProfile> {
         println("wow business logic")
 
-        Promise.value(Unit).map {
-            ProfileManager.profile
-                ?.let { this.onDone(it) }
-                ?: let {
-                    Thread.sleep(5000)
-                    val networkResponse = Profile(
-                        email = "a@a.com",
-                        firstName = "wow",
-                        lastName = "last"
-                    )
-                    ProfileManager.profile = networkResponse
-                    this.onDone(networkResponse)
-                }
-        }
+        return ProfileManager
+            .profile?.let { state.toDone(it) }
+            ?: state.toProfileRequest()
+    }
+
+    override fun onProfileRequest(state: ProfileRequest): Promise<FromProfileRequest> {
+        // network request
+        Thread.sleep(5000)
+        val networkResponse = Profile(
+            email = "a@a.com",
+            firstName = "wow",
+            lastName = "last"
+        )
+        return state.toSaveProfile(networkResponse)
+    }
+
+    override fun onSaveProfile(state: SaveProfile): Promise<FromSaveProfile> {
+        ProfileManager.profile = state.formData
+        return state.toDone(state.formData)
     }
 }
